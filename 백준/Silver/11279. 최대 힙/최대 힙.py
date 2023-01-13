1f5d30f75ec00a70655edca5c11d746bdd70d06e
Kim Minhyuk <63450024+heigarnik@users.noreply.github.com>
2023-01-13 22:48:24 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 50352 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,14 @@
+from queue import PriorityQueue
+import sys
+N = int(sys.stdin.readline().strip())
+pq = PriorityQueue()
+for i in range(N):
+    num = int(sys.stdin.readline().strip())
+    if num == 0:
+        if pq.qsize() == 0:
+            print(0)
+        else:
+            print(pq.get()[1])
+    else:
+        pq.put((-num, num))
+
