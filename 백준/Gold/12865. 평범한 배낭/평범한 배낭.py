f359ee0735d2b57d703d616def567da789418f06
Min Hyuk Kim <63450024+K-MinHyuk@users.noreply.github.com>
2024-03-23 00:47:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 3456 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,20 +1,13 @@
 import sys 
 
 N, K = map(int, sys.stdin.readline().split())
-dp = [[0]*(K+1) for i in range(N+1)]
-item = [[0,0]]
+dp = [0]*(K+1)
+item = []
 max_val = -float('inf')
 
 for i in range(N):
-    item.append(list(map(int, sys.stdin.readline().split())))
+    i_w, i_v = map(int, sys.stdin.readline().split())
+    for w in range(K, i_w-1, -1):
+        dp[w] = max(dp[w], dp[w-i_w] + i_v)
 
-for i in range(N+1):
-    for j in range(K+1):
-        if j == item[i][0] and dp[i-1][j] < item[i][1]:
-            dp[i][j] = item[i][1]
-        elif j > item[i][0] and dp[i-1][j] < (dp[i-1][j-item[i][0]] + item[i][1]):
-            dp[i][j] = dp[i-1][j-item[i][0]] + item[i][1]
-        else:
-            dp[i][j] = dp[i-1][j]
-
-print(dp[N][K])
+print(dp[K])
