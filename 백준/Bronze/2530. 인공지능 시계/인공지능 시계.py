007511f92f2f062c1000f6e5379767c35b48221d
Min Hyuk Kim <63450024+K-MinHyuk@users.noreply.github.com>
2024-11-09 17:42:04 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,23 +1,11 @@
 import sys
 input = sys.stdin.readline
 h, m, s = map(int, input().split(' '))
+cook_time = int(input())
+cur_time = h*3600 + m*60 + s
+r_time = cook_time + cur_time
+r_h = (r_time//3600)%24
+r_m = (r_time%3600)//60
+r_s = (r_time%3600)%60
 
-total = int(input())
-d_h = total//3600
-l_h = total%3600
-d_m = l_h//60
-d_s = l_h%60
-
-r_s = s + d_s
-r_m = m + d_m
-r_h = h + d_h
-
-if r_s >= 60:
-    r_m += 1
-    r_s -= 60
-    
-if r_m >= 60:
-    r_h += 1
-    r_m -= 60
-
-print('{} {} {}'.format((r_h)%24, r_m, r_s))
+print('{} {} {}'.format(r_h, r_m, r_s))
